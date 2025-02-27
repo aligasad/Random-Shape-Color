ed9dd027d9f63625f9bb08379ee1763e1a4b80b2
ASAD ALAM <112319884+aligasad@users.noreply.github.com>
2025-02-27 20:26:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 let round = document.querySelector(".round");
-let btn = document.querySelectorAll("button");
 let shape = document.querySelector("#square");
+let btn = document.querySelectorAll("button");
 
-btn[0].addEventListener("click", changeColor);
-
-function genarateColor(){
+btn[0].addEventListener("click", ()=>{
+    let newColor = `rgb(${makeColor()}, ${makeColor()}, ${makeColor()})`; // 3$ for RGB
+    round.style.backgroundColor = newColor;
+});
+function makeColor(){
     let color = Math.floor(Math.random() * 255);
     return color;
 }
 
-function changeColor(){
-    let newColor = `rgb(${genarateColor()}, ${genarateColor()}, ${genarateColor()})`;
-    round.style.backgroundColor = newColor;
-}
-
-btn[1].addEventListener("click", changeShape);
+btn[1].addEventListener("click", ()=>{
+    let name = randomShape();
+    shape.id = name;
+});
 let arr=["square", "round", "diamond", "triangle", "arrow", "frame", "star","cross", "left-point", "right-point", "parallal", "cheg"];
 
 function randomShape(){
-    let idx = Math.floor(Math.random() * arr.length);
+    let idx = Math.floor(Math.random() * arr.length); // If we take Math.ceil then it generate a number that is greater than length of array 
+    console.log(idx)
     return arr[idx];
-}
-
-function changeShape(){
-    let idname = randomShape();
-    shape.id = idname;
 }
